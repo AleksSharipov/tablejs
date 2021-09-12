c39e02910a2dbceeb4102c34780df5c08da29692
Aleksandr Sharipov <sharipov.sasha.spb@yandex.ru>
2021-09-12 18:27:19 +0300

    add comments

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/**
+ * Получаем переменные, которые в дальнейшем будут нужны. По хорошему, стоит создать отдельный файл 'utils' специально для таких вещей. 
+ */
 const users = document.querySelector('.users');
 const pagination = document.querySelector('.table__nabigation');
 const template = document.querySelector('#table-template').content;
@@ -5,16 +8,30 @@ const buttons = [];
 const sortName = document.querySelector('.sort-name');
 const tableSort = document.querySelector('.table__sort');
 const sortRows = tableSort.querySelectorAll('td');
-//https://alekssharipov.github.io/tablejs/db.json;
-const apiUrl = 'https://alekssharipov.github.io/tablejs/db.json';
-let a;
 
+const newValue = document.querySelector('.form__value')
+const formBtn = document.querySelector('.form__btn');
+const formInputName = document.querySelector('.form__name_value');
+const formInputLastName = document.querySelector('.form__last-name_value');
+const formInputAbout = document.querySelector('.form__about_value');
+const formInputEye = document.querySelector('.form__eye_value');
+
+/**
+ * url для обращения к нашей БД. 
+ * Благодаря тому, что я разместил сайт на github pages можно обратиться к нашей "БД" по url.
+ */
+const apiUrl = 'https://alekssharipov.github.io/tablejs/db.json';
+
+/**
+ * функция для создания таблицы. В ней я обращаюсь к template элементу в разметке, заполняю его данными и добавляю в таблицу users
+ */
 function newCard(obj) {
   const elemTable = template.cloneNode(true);
   const nameElement = elemTable.querySelector('.users__first-name');
   const lastNameElement = elemTable.querySelector('.users__last-name');
   const aboutElement = elemTable.querySelector('.users__about');
   const eyeColorElement = elemTable.querySelector('.users__eye-color');
+  const refactoring = elemTable.querySelector('.users__refactoring');
 
   nameElement.textContent = obj.name.firstName;
   lastNameElement.textContent = obj.name.lastName;
@@ -22,16 +39,20 @@ function newCard(obj) {
   eyeColorElement.textContent = obj.eyeColor;
   eyeColorElement.style.backgroundColor = obj.eyeColor;
 
-  aboutElement.addEventListener('click', function () {
-    a = this.textContent
-    // console.log(this.textContent)
-    console.log(a)
+  refactoring.addEventListener('click', function (e) {
+    const val = this.parentNode;
+    formInputName.textContent = val.querySelector('.users__first-name').textContent;
+    formInputLastName.textContent = val.querySelector('.users__last-name').textContent;
+    formInputAbout.textContent = val.querySelector('.users__about').textContent;
+    formInputEye.textContent = val.querySelector('.users__eye-color').textContent;
   })
 
   users.append(elemTable);
 }
 
-
+/**
+ * Отправляем запрос на сервер с помощью fetch, если приходит ответ с статусом ок, то мы получаем промис к которому обращаемся с помощью json. Если статус ответа отличен от ок, выбрасываем ошибку.
+ */
 
 function getUsers() {
   return fetch(apiUrl)
@@ -43,13 +64,22 @@ function getUsers() {
     })
 }
 
+/*Объединяем все обращения на сервер в один, чтобы воспользоваться promise.all */
 const promiseUser = [getUsers()];
 
+/*Активна ли страница */
 let active;
+
+/*Чисто строк в таблице */
 let notesOnPage = 10;
 
+
+/*
+Создаем promise.all для того, чтобы отобразить все данные только после того, как все обращения к серверы выполнятся. 
+*/
 Promise.all(promiseUser)
   .then(([res]) => {
+    /*Вычисляем ко-во страниц для пагинации */
     let countOfItems = Math.ceil(res.length / notesOnPage);
 
     for (let i = 1; i <= countOfItems; i++) {
@@ -59,13 +89,21 @@ Promise.all(promiseUser)
       buttons.push(li)
     }
 
+    /*по дефолту показываем первых 10 пользователей */
     showTable(buttons[0], res);
 
+    /*
+      при нажатие на кнопку показываем нужные 10 строк
+    */
     for (let button of buttons) {
       button.addEventListener('click', function () {
         showTable(this, res)
       })
     }
+
+    /**
+     * навешиваем слушатель на кнопки сортировки
+     */
 
     for (let i = 0; i < sortRows.length; i++) {
       sortRows[i].addEventListener('click', function () {
@@ -73,7 +111,12 @@ Promise.all(promiseUser)
       })
     }
   })
+  .catch(err => console.log(err))
 
+
+/**
+ * отоюражаем нужную таблицу при перелистывание страниц
+ */
 function showTable(item, res) {
   if (active) {
     active.classList.remove('active');
@@ -95,7 +138,9 @@ function showTable(item, res) {
 
 }
 
-
+/**
+ * функция для сортировки. Принимает таблицу, которую нужно сортировать и колонку для сортировки.
+ */
 function valueSort(sortTable, n) {
   let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.querySelector(sortTable);
@@ -107,6 +152,7 @@ function valueSort(sortTable, n) {
     for (i = 0; i < (rows.length - 1); i++) {
       shouldSwitch = false;
       x = rows[i].querySelectorAll('td')[n];
+
       y = rows[i + 1].querySelectorAll('td')[n];
       if (dir == "asc") {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
